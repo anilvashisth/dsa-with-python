c22933d0cbc84ee6a8b9727cd33e969aca20fd3f
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-23 17:32:34 +0530

    Add files via upload

---
+++ Lecture2Python/Lecture3.py
@@ -0,0 +1,2 @@
+print("Sare Jahaan se Achacha Hindustaan Hamara,Hamara");
+print("This is Python Learning");
---
+++ Lecture2Python/Lecture4.py
@@ -0,0 +1,8 @@
+a = 86;
+b = 34;
+c = a - b;
+d = a * b;
+e = a % b;
+print(" The Difference between a and b is :",c);
+print(" The Multiplication of a and b is :",d);
+print(" The Reminder of a/b is :",  e);
