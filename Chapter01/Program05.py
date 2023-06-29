aefb7bc8770469abd05ef5d0d6377b545d11464e
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-06-29 14:36:48 +0530

    First Commit for Program 5

---
+++ Chapter01/Program05.py
@@ -0,0 +1,11 @@
+a = 1600;
+b = 1220;
+sum = a + b;
+c = a - b;
+d = a * b;
+e = a % b;
+print("The Sum of a and b is :", sum);
+print(" The Difference between a and b is :",c);
+print(" The Multiplication of a and b is :",d);
+print(" The Reminder of a/b is :",  e);
+print("This is Python Program 4")
