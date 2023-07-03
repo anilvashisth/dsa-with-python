835a7bbdf6af6482e5e3167371a02619b6e2de9a
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:40:32 +0530

    First Commit Program 6

---
+++ Chapter01/Program006.py
@@ -0,0 +1,12 @@
+#Program 6 WAP to Convert in Meter,Feet,Inches,Centimeter.
+print("This is Program 6 for 'DSA with Python' Series");
+
+distance = 100;
+meter = distance*1000;
+feet = meter*3.33;
+inches = feet*12;
+centimeter = meter*100;
+print(" The Meter is :", meter);
+print(" The Feet is :", feet);
+print(" The Inches is :", inches);
+print(" The Centimeter is :", centimeter);
