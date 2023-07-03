b9833e57212f8d5b26b3530e94d5a2ad62166dcb
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:37:00 +0530

    Updated Program 2

--- Chapter01/Program002.py
+++ Chapter01/Program002.py
@@ -1,5 +1,7 @@
-a = 67;
-b = 98;
-c = a+b;
-print("Sum of a and b is = ",c);
-print("This is Program 2")
+#Program 2 WAP to Calculate simple interest for given Amount.
+print(" This is Program 2 for 'DSA with Python' Series");
+principalAmount = 200000;
+timeSpan = 2;
+rateofInterest = 12;
+simpleinterest = (principalAmount*timeSpan*rateofInterest)/100;
+print("The Simple Interest is :", simpleinterest);
