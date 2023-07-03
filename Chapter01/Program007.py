77af39b09cf1d19c015379a8c708ad120c7a10c1
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:41:21 +0530

    First Commit Program 7

---
+++ Chapter01/Program007.py
@@ -0,0 +1,14 @@
+#Program 7 WAP to Calculate Aggrigate marks and Percentage of Marks.
+print("This is Program 7 for 'DSA with Python' Series");
+
+Hindi = 95;
+Eng = 94;
+Physics = 96;
+Chemistry = 88;
+Maths = 97;
+aggregatemarks=Hindi+Eng+Physics+Chemistry+Maths;
+totalmarks=100*5;
+percentage=aggregatemarks/totalmarks*100;
+print("The Aggregate marks are:",aggregatemarks);
+print("the Total marks is :",totalmarks);
+print("the Percentage of marks is ",percentage);
