375f63e9941517834aff5b103aa7eaa436b4bbf4
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:42:06 +0530

    First Commit Program 8

---
+++ Chapter01/Program008.py
@@ -0,0 +1,7 @@
+#Program 8 WAP to Convert Temp.
+print("This is Program 8 for 'DSA with Python' Series");
+
+toc = 37 # ("Enter Temp in Calcius:");
+tof = (toc*1.8)+32;
+print("The Temp in Calcius :",toc);
+print("Convert Celcius to Fahrenhiet :",tof);
