db722350806816d63a658046f15ee6123d619c40
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:36:17 +0530

    Update Program 1

--- Chapter01/Program001.py
+++ Chapter01/Program001.py
@@ -1,2 +1,17 @@
-print("Hello Python World");
-print("This is Program 1)
+#Program 1 WAP to Arithmetic Operation for Amount given by user .
+print(" Hello Python World, Sawagat Nahi Karoge Hamara");
+print(" This is Program 1 for 'DSA with Python' Series");
+a = int( input("a = "));
+print("First Number :",a);
+b = int(input("b = "));
+print("Second Number :",b);
+c = int(a+b);
+d = int(a-b);
+e = int(a*b);
+f = float(a/b);
+g = int(a%b);
+print("The Sum of a and b is :", c);
+print("The Difference of a and b is :", d);
+print("The Multiplication of a and b is :", e);
+print("The Division of a and b is :", f);
+print("The Reminder of a and b is :", g);
