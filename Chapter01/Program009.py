c053453e2ff26809b6e5552d05d9df30fd45d546
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2023-07-03 19:42:46 +0530

    First Commit Program 9

---
+++ Chapter01/Program009.py
@@ -0,0 +1,15 @@
+#Program 9 WAP to Print Area,perimeter and Circumference of Rectangle and Circle.
+print("This is Program 9 for 'DSA with Python' Series");
+
+pi=3.14;
+radius = 8;
+length = 78;
+breadth = 22;
+areaofrect = length*breadth;
+periofrect  = 2*(length + breadth);
+circumferenceofcircle=2*pi*radius;
+areaofcircle = pi*radius*radius
+print("Area of Rectangle is :", areaofrect);
+print("Perimeter of Rectangle is :", periofrect);
+print("Cercumference of Cirle :",circumferenceofcircle);
+print("Area of a Circle :",areaofcircle);
